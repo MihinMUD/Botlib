38f34f31df9d4678a73e3cdb49555efb915d46a0
Mihin DIssanayake <73010893+MihinMUD@users.noreply.github.com>
2022-07-07 14:04:11 +0530

    Better error handling

--- Botlib.py
+++ Botlib.py
@@ -1,22 +1,34 @@
+from types import NoneType
+
+
 class Bot:
-    def __init__(self, name:str , desp:str):
+    def __init__(self, name:str , desp:str, showMsg=True):
         self.commands = {}
         self.name = name
         self.desp = desp
+        self.showMsg = showMsg
 
     def onMessage(self, func:callable):
         self.commands[func.__name__] = func
 
     def runBot(self):
-        self.send("I am MUD - "+self.desp)
+        if self.showMsg == True:
+            self.send(f"I am {self.name} - {self.desp}")
         while True:
             req = str(input("User: ")).lower().strip()
             args = req.split()
             if req.startswith("exit"):
                 break
             elif  req != "":
-                command = self.commands.get(args.pop(0))
-                command(args)
+                try:
+                    command = self.commands.get(args.pop(0))
+                    command(args)
+                except TypeError:
+                    print("Invalid Command!")
+                except Exception as e:
+                    print(e)
 
     def send(self, ctx):
         print(f"{self.name}: {ctx}")
+
+        
