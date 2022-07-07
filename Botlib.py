4faffd03a089c74a342192d0c7a5f11cd592e4c8
Mihin DIssanayake <73010893+MihinMUD@users.noreply.github.com>
2022-07-07 13:40:06 +0530

    Added functionality
    More like discordpy!

--- Botlib.py
+++ Botlib.py
@@ -1,28 +1,22 @@
 class Bot:
-    def __init__(self, name:str , desp:str , startMesg:bool = True , customMesg:str = f"Hi i m a") -> None:
+    def __init__(self, name:str , desp:str):
+        self.commands = {}
         self.name = name
-        self.desp =  desp
-        self.customMesg = customMesg
-        
-    def onMessage(self, ctx:str):
-            pass
+        self.desp = desp
+
+    def onMessage(self, func:callable):
+        self.commands[func.__name__] = func
 
     def runBot(self):
+        self.send("I am MUD - "+self.desp)
         while True:
-            req = str(input("User: "))
-            if req == "exit":
+            req = str(input("User: ")).lower().strip()
+            args = req.split()
+            if req.startswith("exit"):
                 break
-            elif  req.lower() != "":
-                self.onMessage(req)
-    
-    def command(self, func:callable):
-        cmdName = func.__name__
-        def onMessage(ctx):
-            if ctx.lower() == cmdName.lower():
-                func(ctx)
-        self.onMessage = onMessage
-    
+            elif  req != "":
+                command = self.commands.get(args.pop(0))
+                command(args)
+
     def send(self, ctx):
         print(f"{self.name}: {ctx}")
-
-        
